a4206e92fe79f686886b79394f978a2ef8c271e2
Stelastack <5833498+Stelastack@users.noreply.github.com>
2023-08-22 14:00:00 -0600

    Create SQL schema for tasks table with columns

--- backend/db.js
+++ backend/db.js
@@ -8,6 +8,17 @@ db.serialize(() => {
     email TEXT UNIQUE NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
   )`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT,
+    status TEXT DEFAULT 'todo',
+    assigned_to INTEGER,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (assigned_to) REFERENCES users (id)
+  )`);
 });
 
 module.exports = db;

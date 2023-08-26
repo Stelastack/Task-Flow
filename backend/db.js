2fead5c9aea724f7c831aa12d906c4ec9eb6fe01
Stelastack <5833498+Stelastack@users.noreply.github.com>
2023-08-26 10:00:00 -0600

    Add password column to users table

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,7 @@ db.serialize(() => {
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
   )`);
 

9d76946c6610c1464834bc422a5adcd97f5e9db6
Stelastack <5833498+Stelastack@users.noreply.github.com>
2023-08-29 13:00:00 -0600

    Add user authentication with login and password hashing

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // GET /users
 router.get('/', (req, res) => {
@@ -12,8 +14,9 @@ router.get('/', (req, res) => {
 
 // POST /users
 router.post('/', (req, res) => {
-  const { name, email } = req.body;
-  db.run('INSERT INTO users (name, email) VALUES (?, ?)', [name, email], function(err) {
+  const { name, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  db.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword], function(err) {
     if (err) return res.status(500).json({ error: err.message });
     res.json({ id: this.lastID });
   });
@@ -21,8 +24,16 @@ router.post('/', (req, res) => {
 
 // PUT /users/:id
 router.put('/:id', (req, res) => {
-  const { name, email } = req.body;
-  db.run('UPDATE users SET name = ?, email = ? WHERE id = ?', [name, email, req.params.id], (err) => {
+  const { name, email, password } = req.body;
+  let query = 'UPDATE users SET name = ?, email = ?';
+  let params = [name, email];
+  if (password) {
+    query += ', password = ?';
+    params.push(bcrypt.hashSync(password, 10));
+  }
+  query += ' WHERE id = ?';
+  params.push(req.params.id);
+  db.run(query, params, (err) => {
     if (err) return res.status(500).json({ error: err.message });
     res.json({ message: 'User updated' });
   });
@@ -33,6 +44,17 @@ router.delete('/:id', (req, res) => {
   db.run('DELETE FROM users WHERE id = ?', [req.params.id], (err) => {
     if (err) return res.status(500).json({ error: err.message });
     res.json({ message: 'User deleted' });
+  });
+});
+
+// POST /users/login
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  db.get('SELECT * FROM users WHERE email = ?', [email], (err, user) => {
+    if (err) return res.status(500).json({ error: err.message });
+    if (!user || !bcrypt.compareSync(password, user.password)) return res.status(400).json({ error: 'Invalid credentials' });
+    const token = jwt.sign({ id: user.id }, 'secret');
+    res.json({ token });
   });
 });
 

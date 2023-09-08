014e70f590662c9c0a9ffb6eddd1aa152f217de5
Stelastack <5833498+Stelastack@users.noreply.github.com>
2023-09-08 15:00:00 -0600

    Add user authentication UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
+import Login from './components/Login';
 import KanbanBoard from './components/KanbanBoard';
 
 function App() {
+  const [token, setToken] = useState(localStorage.getItem('token'));
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setToken(null);
+  };
+
+  if (!token) return <Login onLogin={setToken} />;
+
   return (
     <div className="min-h-screen bg-gray-100">
-      <header className="bg-blue-600 text-white p-4">
+      <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
         <h1 className="text-2xl font-bold">Task Flow</h1>
+        <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded">Logout</button>
       </header>
       <main>
         <KanbanBoard />
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const Login = ({ onLogin }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/users/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await res.json();
+    if (res.ok) {
+      localStorage.setItem('token', data.token);
+      onLogin(data.token);
+    } else {
+      alert(data.error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-md w-80">
+        <h2 className="text-2xl mb-4">Login</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full mb-2 p-2 border"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full mb-4 p-2 border"
+          required
+        />
+        <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;

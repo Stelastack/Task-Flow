68dc6f5d0d6153128508a1bb2ec2a1c994b94c8b
Stelastack <5833498+Stelastack@users.noreply.github.com>
2023-09-12 11:00:00 -0600

    Create unit tests for API endpoints

--- backend/tests/tasks.test.js
+++ backend/tests/tasks.test.js
@@ -6,4 +6,9 @@ describe('Tasks API', () => {
     const res = await request(app).get('/api/tasks');
     expect(res.status).toBe(401);
   });
+
+  it('should return 401 for POST without auth', async () => {
+    const res = await request(app).post('/api/tasks').send({ title: 'Test' });
+    expect(res.status).toBe(401);
+  });
 });
